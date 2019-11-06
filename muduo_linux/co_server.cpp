34b105b7139924ffc3607a9e53c12a37e10589bf
余靖 <122079328@qq.com>
2019-11-06 17:06:24 +0800

    .

--- muduo_linux/co_server.cpp
+++ muduo_linux/co_server.cpp
@@ -1,18 +1,12 @@
-﻿
-#include"coloop.h"
+﻿#include"coloop.h"
 #include"ksocket.h"
+#include"httprequest.h"
+#include"httpresponse.h"
+#include"ktimer.h"
+#include"buffer.h"
 
 #include<unistd.h>
-#include<string>
-
-std::string msg =
-"HTTP/1.1 200 OK\r\n"
-"Connection: keep-alive\r\n"
-"Content-Length: 13\r\n"
-"Content-Type: text/plain\r\n"
-"Server: RJingYu\r\n"
-"\r\n"
-"hello, world\n";
+#include<fcntl.h>
 
 struct connection {
 
@@ -21,24 +15,124 @@ struct connection {
 		delete cpt_;
 	}
 
+	static std::vector<connection*> conmap_;
+
 	ksocket* sock_;
 	coloop::coloop_item* cpt_;
 
 };
 
+std::vector<connection*> connection::conmap_;
+
+void httpCallback(const httprequest& request, httpresponse& response) {
+	response.addHeader("Server", "RJingYu");
+	if (request.getPath() == "/") {
+		response.setStatu1(httpresponse::k200OK);
+		response.setStatu2("OK");
+		response.addHeader("Content-Type", "text/html");
+		int fd = open("./html/index.html", O_RDONLY);
+		char buf[1024];
+		ssize_t nread = 0;
+		while ((nread = read(fd, buf, 1024)) > 0)
+			response.getBody().append(buf, nread);
+		close(fd);
+	}
+	else {
+		string path = "/home/rjingyu/html" + request.getPath();
+		int fd = open(path.c_str(), O_RDONLY);
+		if (fd < 0) {
+			response.setStatu1(httpresponse::k404NotFound);
+			response.setStatu2("Not Found");
+			response.setKeepAlive(0);
+		}
+		else {
+			response.setStatu1(httpresponse::k200OK);
+			response.setStatu2("OK");
+
+			size_t i = 0;
+			string head = request.getHeader("Accept");
+			while (i != head.size() && head[i] != ',')
+				++i;
+			response.addHeader("Content-Type", string(head.begin(), head.begin() + i));
+
+			char buf[1024];
+			ssize_t nread = 0;
+			while ((nread = read(fd, buf, 1024)) > 0)
+				response.getBody().append(buf, nread);
+			close(fd);
+		}
+	}
+}
+
+void sendBuff(connection* con, buffer* buff) {
+	ssize_t nwrote;
+	int fd = con->cpt_->getFd();
+	while (1) {
+		nwrote = write(fd, buff->beginPtr(), buff->usedBytes());
+		if (nwrote <= 0) {
+			coroutine::yield();
+			continue;
+		}
+		buff->retrieve(nwrote);
+
+		if (buff->usedBytes()) {
+			if (con->cpt_->isWriting())
+				coroutine::yield();
+			else {
+				con->cpt_->disableReading();
+				con->cpt_->enableWriting();
+				con->cpt_->updateEvents();
+			}
+		}
+		else
+			break;
+	}
+	if (con->cpt_->isWriting()) {
+		con->cpt_->disableWrting();
+		con->cpt_->enableReading();
+		con->cpt_->updateEvents();
+	}
+}
+
 void connect_handler(connection* con) {
 	ssize_t nread;
-	char buf[1024];
-	while (1) {
-		nread = read(con->sock_->getFd(), &buf, 1024);
-		if (nread == 0)
-			break;
+	buffer buff;
+	httprequest request;
+	int fd = con->cpt_->getFd();
 
-		write(con->sock_->getFd(), msg.c_str(), msg.size());
-		con->cpt_->setTimeout(6666);
+	printf("建立一个连接\n");
+	while (1) {
+		while ((nread = read(fd, buff.endPtr(), 1024)) > 0) {
+			buff.hasUsed(nread);
+			buff.ensureLeftBytes(1024);
+		}
+
+		if (nread == 0 || !request.praseRequest(&buff))
+			break;
+		else if (request.praseDone()) {
+			string temp = request.getHeader("Connection");
+			bool alive = (temp == "keep-alive") ||
+				(request.getVersion() == httprequest::kHTTP11 && temp != "close");
+
+			httpresponse response(alive);
+			httpCallback(request, response);
+
+			buff.retrieve(request.getLength());
+			request.reset();
+
+			buffer buff2;
+			response.appendToBuffer(&buff2);
+			sendBuff(con, &buff2);
+
+			if (!response.keepAlive()) {
+				con->sock_->shutdownWrite();
+				break;
+			}
+		}
 		coroutine::yield();
 	}
-	printf("客户端关闭连接\n");
+	printf("关闭一个连接\n");
+	connection::conmap_.push_back(con);
 }
 
 void accept_handler(connection* con) {
@@ -52,10 +146,16 @@ void accept_handler(connection* con) {
 			temp->sock_ = new ksocket(clifd, cliaddr);
 			temp->cpt_ = new coloop::coloop_item(clifd, std::bind(connect_handler, temp));
 			temp->cpt_->enableReading();
+			temp->cpt_->enableEpollet();
 			temp->cpt_->updateEvents();
 
 			continue;
 		}
+
+		for (auto ptr : connection::conmap_)
+			delete ptr;
+		connection::conmap_.clear();
+
 		coroutine::yield();
 	}
 }
