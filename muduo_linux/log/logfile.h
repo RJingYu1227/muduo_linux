95de75e40bd63ae44e4690da291c75dba248d074
余靖 <122079328@qq.com>
2019-12-19 15:01:54 +0800

    .

---
+++ muduo_linux/log/logfile.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include<string>
+
+class appendfile;
+
+class logfile {
+public:
+
+	logfile(const char* basename, off_t rollsize, int count_limit = 1024);
+	~logfile();
+
+	void append(const char* data, size_t len);
+	void flush();
+
+private:
+	
+	void rollfile();
+	void setLogFileName(std::string& str);
+	void append_unlock(const char* data, size_t len);
+
+	std::string basename_;
+	appendfile* file_;
+	off_t rollsize_;
+
+	int count_;
+	int count_limit_;
+	long int last_roll_;
+
+	static const int kPeriod = 60 * 60;
+
+};
---
+++ muduo_linux/log/logging.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include"logstream.h"
+
+#include<errno.h>
+#include<functional>
+
+class asynclogger;
+
+class logger {
+public:
+	typedef std::function<void(const s_logbuffer&)> functor;
+
+	logger(const char* filename, int line);
+	~logger();
+
+	logstream& stream() { return impl_.stream_; }
+
+	static std::string getFilenname() { return log_filename_; }
+	static void setFilename(const char* filename) { log_filename_ = filename; }
+
+	static void setOutput(const functor& func) { output = func; }
+	static void setOutput(functor&& func) { output = std::move(func); }
+
+	static void createAsyncLogger();
+
+private:
+
+	static std::string log_filename_;
+
+	static functor output;
+	static void defaultOutput(const s_logbuffer&);
+
+	struct impl {
+
+		impl(const char* basename, int line);
+
+		logstream stream_;
+		std::string basename_;
+		int line_;
+
+	};
+
+	impl impl_;
+
+};
+
+#define LOG logger(__FILE__, __LINE__).stream()
